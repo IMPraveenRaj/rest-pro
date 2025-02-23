32a87612dbd73552bba8b87f11023b353c363a81
IMPraveenRaj <impraveen13@gmail.com>
2025-02-23 18:29:30 +0100

    rest-pro changes

---
+++ src/main/java/com/springheaven/restpro/restpro/RestProApplication.java
@@ -0,0 +1,13 @@
+package com.springheaven.restpro.restpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springheaven/restpro/restpro/RestProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springheaven.restpro.restpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
